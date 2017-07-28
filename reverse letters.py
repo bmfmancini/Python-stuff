3c1351de97383403d977926f6b62fcccb34813a5
bmfmancini <seanmancini2010@gmail.com>
2017-07-28 13:17:01 -0400

    Add files via upload

---
+++ reverse letters.py
@@ -0,0 +1,17 @@
+
+word = "hello"
+
+
+def backward (word):
+    index = len(word)
+    while index > 0:
+        letter = word[index -1]
+        print (letter)
+        index = index -1 
+
+ 
+        
+
+
+backward(word)
+
